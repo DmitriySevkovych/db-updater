71e0b781575fba5e106c663c85560834081f4ea0
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2020-02-21 11:17:38 +0100

    Add ODBC method for income table

--- src/db/writer_homedb.py
+++ src/db/writer_homedb.py
@@ -9,11 +9,19 @@ class HomeDBWriter(Writer):
         sqlInsert = f"INSERT INTO expenses {self._get_expense_attributes()} VALUES {self._get_expense_placeholders()}"
         sqlUpdate = f"UPDATE ref_blueprint SET last_update='{date.today()}' WHERE key = ?"
 
-        Writer().execute(db_file,sqlInsert,_get_expense_values(blueprint))
-        Writer().execute(db_file,sqlUpdate,(blueprint.key,))
+        writer = Writer()
+        #writer.execute(db_file,sqlInsert,self._get_expense_values(blueprint))
+        writer.execute(db_file,sqlUpdate,(blueprint.key,))
 
-    def write_income(self):
-        pass
+    def write_income(self, blueprint: Blueprint):
+        db_file = os.getenv("DB_FILE")
+        sqlInsert = f"INSERT INTO income {self._get_income_attributes()} VALUES {self._get_income_placeholders()}"
+        sqlUpdate = f"UPDATE ref_blueprint SET last_update='{date.today()}' WHERE key = ?"
+
+        writer = Writer()
+        #writer.execute(db_file,sqlInsert,self._get_income_values(blueprint))
+        writer.execute(db_file,sqlUpdate,(blueprint.key,))
+
 
     # Private helper functions
     def _get_expense_attributes(self) -> str:
@@ -35,11 +43,18 @@ class HomeDBWriter(Writer):
             , os.getenv('AGENT')
             ])
 
-    def get_income_attributes(self):
-        return ""
+    def _get_income_attributes(self) -> str:
+        return "(date, type, amount, origin, payment_to, agent)"
 
-    def get_income_placeholders(self):
-        return ""
+    def _get_income_placeholders(self) -> str:
+        return "(?, ?, ?, ?, ?, ?)"
 
-    def get_income_values(self):
-        return ""
+    def _get_income_values(self, blueprint: Blueprint) -> tuple:
+        return tuple([ 
+            '#TODO: date'
+            , self.transaction_type
+            , self.amount
+            , self.origin
+            , self.target_bank_account
+            , os.getenv('AGENT')
+            ])
