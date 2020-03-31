09d4fe239fe9e4f8cb46d2c15b858aab4aa091e0
Dmitriy Sevkovych <d.sevkovych@web.de>
2020-03-31 22:57:05 +0200

    Add logging and lint main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import os
-from dotenv import load_dotenv
-
 import logging
+
 from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
 
-from db.reader_homedb import *
-from db.writer_homedb import *
-from utils.scheduler import *
-from utils.mailing import *
+from db.reader_homedb import HomeDBReader
+from db.writer_homedb import HomeDBWriter
+from utils.scheduler import schedule_transactions
+from utils.mailing import send_summary
 
 
 def synchronize_db():
@@ -21,7 +21,8 @@ def synchronize_db():
     path = Path(db_file)
     logging.basicConfig(
         filename=f'{path.parent}/{path.stem}.log', level=logging.DEBUG)
-    logging.info(f'Working with database {db_file}')
+    logging.info('DB-UPDATER LAUNCHED ON %s', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    logging.debug('Working with database %s', db_file)
     logging.debug('Start processing')
 
     home_db_reader = HomeDBReader()
@@ -29,21 +30,27 @@ def synchronize_db():
 
     # Sync expenses
     for blueprint in home_db_reader.get_blueprints('expense'):
+        logging.info('Start working on the expense blueprint %s', blueprint.key)
         for transaction in schedule_transactions(blueprint):
             home_db_writer.write_expenses(transaction)
         home_db_writer.update_blueprint(blueprint)
+        logging.debug('Blueprint %s done!', blueprint.key)
 
     # Sync income
     for blueprint in home_db_reader.get_blueprints('income'):
+        logging.info('Start working on the income blueprint %s', blueprint.key)
         for transaction in schedule_transactions(blueprint):
             home_db_writer.write_income(transaction)
         home_db_writer.update_blueprint(blueprint)
+        logging.debug('Blueprint %s done!', blueprint.key)
 
     # Send mail with synchronisation summary
+    logging.info('Preparing to send an update summary to %s', os.getenv("EMAIL_RECEIVERS"))
     send_summary(home_db_writer.get_summary())
+    logging.debug('Sending update summary done!')
 
     logging.debug('End processing')
-    print('end')
+    print('Database successfully updated')
 
 
 if __name__ == "__main__":
