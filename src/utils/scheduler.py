b6a9cd4866c59d67f62ad4d45a9ab498e6952d38
Dmitriy Sevkovych <d.sevkovych@web.de>
2020-03-31 23:07:38 +0200

    Add logging and lint scheduler.py

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -1,8 +1,8 @@
 import logging
 import datetime
 
-from db.model import *
-from utils.datecheck import *
+from db.model import Blueprint, Transaction
+from utils.datecheck import get_date_from_str, is_payment_due, get_next_transaction_date
 
 
 def schedule_transactions(blueprint: Blueprint) -> list:
@@ -10,25 +10,26 @@ def schedule_transactions(blueprint: Blueprint) -> list:
     schedule = []
 
     today = datetime.datetime.now().date()
-    if(blueprint.last_update):
+    if blueprint.last_update:
         days_passed = today - get_date_from_str(blueprint.last_update)
         delta = days_passed.days
     else:
         delta = 1
-        logging.warn(f'The blueprint {blueprint} was never updated before!')
+        logging.warning(
+            'The blueprint %s was never updated before!', blueprint.key)
 
-    logging.info(f'Scheduler will have to check the last {delta} days.')
+    logging.info('Scheduler will have to check the last %s days.', delta)
 
     dates_to_check = [
         today - datetime.timedelta(days=x) for x in range(delta)]
 
     for check_date in dates_to_check:
-        if(is_payment_due(check_date, blueprint)):
+        if is_payment_due(check_date, blueprint):
             transaction_date = get_next_transaction_date(check_date)
             schedule.append(Transaction(transaction_date, blueprint))
-            logging.debug(
-                f'Payment for {blueprint.key} is due on {check_date} and scheduled for {transaction_date}')
+            logging.debug('Payment for %s is due on %s and scheduled for %s',
+                          blueprint.key, check_date, transaction_date)
 
     logging.info(
-        f'Scheduled {len(schedule)} transactions for blueprint {blueprint}')
+        'Scheduled %s transactions for blueprint %s', len(schedule), blueprint.key)
     return schedule
